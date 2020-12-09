a7f750002f0dd24865c4726ae302b2ae6ac840b1
Bill Sourour <bill.sourour@arcnovus.com>
2020-12-09 11:13:16 -0500

    fix FOUC?

--- websites/devmastery.com/src/components/AppLayout.tsx
+++ websites/devmastery.com/src/components/AppLayout.tsx
@@ -3,25 +3,6 @@ import Image from "next/image";
 import Menu from "./Menu";
 import { PropsWithChildren } from "react";
 
-const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-    font-size: 18px;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-`;
-
 const Container = styled.div`
   display: grid;
   width: 100vw;
@@ -72,20 +53,17 @@ export default function AppLayout({
   text,
 }: PropsWithChildren<{ text?: { menu: object; footer: object } }>) {
   return (
-    <>
-      <GlobalStyle />
-      <Container>
-        <Header>
-          <TopBar>
-            <Logo>
-              <Image src="/images/logo copy.svg" width={200} height={30} />
-            </Logo>
-            <Menu text={text?.menu} />
-          </TopBar>
-        </Header>
-        <Body>{children}</Body>
-        <Footer>{text?.footer["copyright"]}</Footer>
-      </Container>
-    </>
+    <Container>
+      <Header>
+        <TopBar>
+          <Logo>
+            <Image src="/images/logo copy.svg" width={200} height={30} />
+          </Logo>
+          <Menu text={text?.menu} />
+        </TopBar>
+      </Header>
+      <Body>{children}</Body>
+      <Footer>{text?.footer["copyright"]}</Footer>
+    </Container>
   );
 }
--- websites/devmastery.com/src/pages/_app.tsx
+++ websites/devmastery.com/src/pages/_app.tsx
@@ -1,10 +1,32 @@
 import { AppProps } from "next/app";
 import AppLayout from "../components/AppLayout";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-size: 18px;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <AppLayout {...pageProps}>
-      <Component {...pageProps} />
-    </AppLayout>
+    <>
+      <GlobalStyle />
+      <AppLayout {...pageProps}>
+        <Component {...pageProps} />
+      </AppLayout>
+    </>
   );
 }
