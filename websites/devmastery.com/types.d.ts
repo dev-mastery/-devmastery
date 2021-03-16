0db1a1e4de22b1ac144fff0502d64eaa92a6debe
Bill Sourour <bill.sourour@arcnovus.com>
2021-03-16 12:56:26 -0400

    silence linter

--- websites/devmastery.com/types.d.ts
+++ websites/devmastery.com/types.d.ts
@@ -2,6 +2,7 @@ import "@emotion/react";
 import type { Theme as SiteTheme } from "./src/theme";
 
 declare module "@emotion/react" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Theme extends SiteTheme {}
 }
 export type Awaited<T> = T extends PromiseLike<infer U> ? Awaited<U> : T;
